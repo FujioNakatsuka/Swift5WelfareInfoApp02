959fbe13fd2d4e39aedd95b89d471e997943f09f
Nakatsuka Fujio <ichifuji.2taka@ezweb.ne.jp>
2020-12-29 15:16:46 +0900

    Initial Commit

---
+++ Swift5WelfareInfoApp02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift5WelfareInfoApp02
+//
+//  Created by 中塚富士雄 on 2020/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
